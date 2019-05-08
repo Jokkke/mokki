54a3c193150e352037bc2b173e3298f5dc95ccaa
Joel Hietala <Joel.Hietala@edu.savonia.fi>
2019-05-08 19:59:24 +0300

    Lisätty käyttäjienhallinta + pieniä muutoksia. Valmis käytettäväks.

--- MokkiApp/MokkiApp/FrmMain.cs
+++ MokkiApp/MokkiApp/FrmMain.cs
@@ -43,6 +43,9 @@ namespace MokkiApp
             dragging = false;
         }
 
-        
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
